e30bea6adfd634806c4b57c486e9a6c2d5164917
Nguyen Tran <nguyentd-uet@users.noreply.github.com>
2017-07-13 18:50:22 +0700

    Update configfb.php
    m

--- configfb.php
+++ configfb.php
@@ -3,8 +3,8 @@ session_start();
 set_time_limit(0);
 require_once 'src/Facebook/autoload.php';
 $fb = new Facebook\Facebook([
-			'app_id' => '255778004872509',
-			'app_secret' => '3dff8b00119ab28e894d0c76e5f5e390',
+			'app_id' => '',
+			'app_secret' => '',
 			'default_graph_version' => 'v2.5',
 			]);
 $oAuth2Client = $fb->getOAuth2Client();
